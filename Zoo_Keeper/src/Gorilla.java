f36e11e617ffbd15aa62889a5ac8bdd49c6c6318
Donatela Qesari <donatelaqesari@gmail.com>
2022-06-16 18:26:44 +0200

    another commit

---
+++ Zoo_Keeper/src/Gorilla.java
@@ -0,0 +1,16 @@
+public class Gorilla extends Mammal {
+    public void throwSomething() {
+        System.out.println("Gorilla has thrown something.");
+        energyLevel -= 5;
+    }
+
+    public void eatBananas() {
+        System.out.println("Gorilla ate a banana!");
+        energyLevel += 10;
+    }
+
+    public void climb() {
+        System.out.println("Gorilla has climbed a tree.");
+        energyLevel -= 10;
+    }
+}
---
+++ Zoo_Keeper/src/Mammal.java
@@ -0,0 +1,8 @@
+public class Mammal {
+    Integer energyLevel = 100;
+
+    public Integer displayEnergy() {
+        System.out.println("Energy level is: " + energyLevel);
+        return energyLevel;
+    }
+}
