a1c7271d3e8fea95145318830264369464e5e9c9
Donatela Qesari <donatelaqesari@gmail.com>
2022-06-21 20:11:23 +0200

    added Daikichi

---
+++ daikichiPathVariable/src/main/java/com/javastack/spring/daikichipathvariable/controllers/DaikichiController.java
@@ -0,0 +1,21 @@
+package com.javastack.spring.daikichipathvariable.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //accept request from the client
+@RequestMapping("/daikichi")
+public class DaikichiController {
+    @RequestMapping("")
+    public String daikichi(){
+        return "Welcome!";
+    }
+    @RequestMapping("/today")
+    public String daikichiToday(){
+        return "Today you will find luck in all your endeavors!";
+    }
+    @RequestMapping("/tomorrow")
+    public String daikichiTomorrow(){
+        return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+    }
+}
